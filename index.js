850e974f182ce0576a3c8a0b659d3564e68df9a2
alexey-nextreason <68570363+alexey-nextreason@users.noreply.github.com>
2021-08-12 14:37:26 -0500

    Update index.js
    Reverting renderFiles change

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ async function run() {
     core.debug(`env: KUBECONFIG="${process.env.KUBECONFIG}"`);
 
     // Render value files using github variables.
-    await renderFiles(valueFiles.concat([]), {
+    await renderFiles(valueFiles.concat(["./values.yml"]), {
       secrets,
       deployment: context.payload.deployment,
     });
